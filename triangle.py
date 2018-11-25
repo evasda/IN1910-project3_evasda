2f34533bb1ca2dfeb0cfcd206b0e98b787f06485
evasda <evasda@math.uio.no>
2018-11-25 17:52:41 +0100

    Added 1c) as function with input iterations. Also added some code for plotting 1c).

--- triangle.py
+++ triangle.py
@@ -28,16 +28,31 @@ def tri(n):
 	x_0 = []
 	r = []
 	for i in range(n):
-		""" Generates 1000 points inside the triangle. """
+		""" Generates 1000 points inside the triangle. Takes the number of iterations as input, returns list of points."""
 		rand = random.random(3)
 		r += [rand/(rand[0]+rand[1]+rand[2])]
 		x_0 += [corners[0]*r[i][0] + corners[1]*r[i][1] + corners[2]*r[i][2]]
 	return(x_0)
-
+"""
 plt.scatter(*zip(*tri(1000)))
 plt.axis('equal')
 plt.show()
+"""
 
 #1c)
 
-np.random.randint()
+def points(n):
+	""" Takes the number of iterations as input, and returns the sequence of points as a list."""
+	j= np.random.randint(3)
+	x2 = []
+	x2 += tri(1)
+	for i in range(1,n+6):						# n+1+1 as we want to remove the first 5
+		j = np.random.randint(3)
+		x2 += [(x2[i-1] - corners[j])/2]
+	return(x2[4:])
+
+
+
+plt.scatter(*zip(*points(10000)), s=0.1)
+plt.axis('equal')
+plt.show()
